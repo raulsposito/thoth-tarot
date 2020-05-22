ac4711be9bd540a2460fca1da236c75427dd9635
Raul Sposito <raulsposito@gmail.com>
2020-05-22 16:03:10 +0000

    added card class

---
+++ lib/card.rb
@@ -0,0 +1,30 @@
+class Card 
+  
+  attr_accessor :name, :instict, :purpose, :light, :shadow, :qualities
+  
+  @@all = []
+  
+  def initialize(card_hash)
+    card_hash.each do |attribute , value|
+      self.send("#{attribute}=", value)
+    end
+    @@all << self
+  end
+  
+  def self.create_from_collection(card_array)
+    card_array.each do |card_hash|
+      Card.new(card_hash)
+    end
+  end
+  
+  def add_card_attributes(attributes_hash)
+    attributes_hash.each do |attr, value|
+      self.send("#{attr}=", value)
+    end
+    self
+  end
+  
+  def self.all
+    @all 
+  end
+end
