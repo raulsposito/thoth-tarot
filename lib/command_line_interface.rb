dea9095590612a1746bcadce97d0efd7527d72a8
Raul Sposito <raulsposito@gmail.com>
2020-05-28 18:01:04 +0000

    updated CLI functionalities

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -15,7 +15,28 @@ class CommandLineInterface
   end
   
   def make_cards
-  
+    card_array = CardScraper.scrape_index_page(BASE_PATH + 'index.html')
+    CardScraper.create_from_collection(card_array)
   end
+  
+  def add_cards_attributes
+    Card.all.each do |c|
+      attributes = CardScraper.scrape_profile_page(BASE_PATH + c.profile_url)
+      c.add_cards_attributes(attributes)
+    end
+  end
+  
+  def display_cards
+    Card.all.each do |c|
+      puts "#{c.name.upcase}".colorize(:purple)
+      puts " Instinct:".colorize(:light_blue) + " #{c.instinct}"
+      puts " Purpose:".colorize(:light_blue) + " #{c.purpose}"
+      puts " The Light".colorize(:light_blue) + " #{c.the_light}"
+      puts " The Shadow".colorize(:light_blue) + " #{c.the_shadow}"
+      puts " Qualities".colorize(:light_blue) + " #{c.qualities}"
+      puts "*******************".colorize(:red)
+    end
+  end
+  
   
 end
