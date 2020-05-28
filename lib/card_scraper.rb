05feb5bf764f789d1085718f003d055f05c78908
Raul Sposito <raulsposito@gmail.com>
2020-05-28 18:38:10 +0000

    initial index scraper added

--- lib/card_scraper.rb
+++ lib/card_scraper.rb
@@ -2,4 +2,13 @@ require 'open-uri'
 
 class CardScraper
   
-end  
+  def self.scrape_index_page(index_url)
+    index_url = Nokogiri::HTML(open(index_url))
+    card = []
+    index_page.css("table#tablepress-95.tablepress.tablepress-id-95 tr td center a img").each do |c| 
+      card_name = "#{c.attr('alt').text.strip}"
+      card << {name: card_name}
+    end
+  end 
+  
+end
