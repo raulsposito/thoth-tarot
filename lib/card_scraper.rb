722f4ca4162fe519c55c7d352058bdc2cd91891a
Raul Sposito <raulsposito@gmail.com>
2020-05-29 00:28:18 +0000

    refactored scrape_index_page method

--- lib/card_scraper.rb
+++ lib/card_scraper.rb
@@ -1,14 +1,19 @@
+require 'nokogiri'
 require 'open-uri'
 
 class CardScraper
   
   def self.scrape_index_page(index_url)
-    index_url = Nokogiri::HTML(open(index_url))
+    doc = Nokogiri::HTML(open(index_url))
     card = []
-    index_page.css("table#tablepress-95.tablepress.tablepress-id-95 tr td center a img").each do |c| 
-      card_name = "#{c.attr('alt').text.strip}"
-      card << {name: card_name}
+    
+    doc.css('.tablepress td').each do |td|
+      if td.css('img').attribute('alt')
+        puts td.css('img').attribute('alt').value
+      end
+      puts td.css('a').attribute('href').value
     end
+    
   end 
   
 end
