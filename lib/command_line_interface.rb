498b5220a42a92d4339ffeffeaf038e73a844264
Raul Sposito <raulsposito@gmail.com>
2020-06-04 14:49:33 +0000

    updated command_line_interface.rb

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,9 +1,3 @@
-require relative '../lib/card.rb'
-require relative '../lib/card_scraper.rb'
-
-require "nokogiri"
-require "colorize"
-
 class CommandLineInterface 
   BASE_PATH = "https://tarotx.net/tarot-card-meanings/thoth/"
   
