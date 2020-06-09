179d7a6f4da61cbfac8fc8d02197f91d68898083
Raul Sposito <raulsposito@gmail.com>
2020-06-09 00:13:57 +0000

    fixed syntax error

--- lib/thoth_tarot/card_scraper.rb
+++ lib/thoth_tarot/card_scraper.rb
@@ -3,19 +3,17 @@ class ThothTarot::CardScraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     card = []
-    
     doc.css('.tablepress td').each do |td|
       if td.text != ""
-      if td.css('img').attribute('alt')
-        puts td.css('img').attribute('alt').value
-      else puts td.css('a').attribute('href').value
+        if td.css ('img').attribute('alt')
+          puts td.css('img').attribute('alt').value
+        else puts td.css('a').attribute('href').value
+      end
     end
   end
   
-  
   def self.scrape_card_profile(card_profile)
-    #here goes the code to scrape each indivitual card info
+    #code 
   end
-  
   
 end
