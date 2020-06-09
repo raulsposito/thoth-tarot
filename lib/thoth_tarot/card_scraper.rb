ce31038a114d72d08a605b4d64822995aa48e3a2
Raul Sposito <raulsposito@gmail.com>
2020-06-09 22:56:57 +0000

    fixed first level scraper to parse better

--- lib/thoth_tarot/card_scraper.rb
+++ lib/thoth_tarot/card_scraper.rb
@@ -3,14 +3,16 @@ class ThothTarot::CardScraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     card = []
-    doc.css('.tablepress td').each do |td|
-      if td.text != ""
-        if td.css ('img').attribute('alt')
-          puts td.css('img').attribute('alt').value
-        else puts td.css('a').attribute('href').value
+    doc.css('.tablepress td').each do |card|
+      if card.text != ""
+        if card.css('img').attribute('alt')
+          puts card.css('img').attribute('alt').value 
+        else puts card.css('a').attribute('href').value.to_s.delete_prefix!("https://tarotx.net/tarot-card-meanings/thoth/").chomp('.html').split('-').join(' ').capitalize
       end
     end
   end
+  
+  
   
   def self.scrape_card_profile(card_profile)
     cards = {}
