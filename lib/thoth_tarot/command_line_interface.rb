a6b6cbb106186164a326d07576024526ccc2dbcf
Raul Sposito <raulsposito@gmail.com>
2020-06-09 21:14:38 +0000

    refactor

--- lib/thoth_tarot/command_line_interface.rb
+++ lib/thoth_tarot/command_line_interface.rb
@@ -11,18 +11,18 @@ class ThothTarot::CommandLineInterface
   
   def make_cards
     card_array = ThothTarot::CardScraper.scrape_index_page(BASE_PATH + 'index.html')
-    CardScraper.create_from_collection(card_array)
+    ThothTarot::CardScraper.create_from_collection(card_array)
   end
   
   def add_cards_attributes
-    Card.all.each do |c|
+    ThothTarot::Card.all.each do |c|
       attributes = ThothTarot::CardScraper.scrape_profile_page(BASE_PATH + c.profile_url)
       c.add_cards_attributes(attributes)
     end
   end
   
   def display_cards
-    Card.all.each do |c|
+    ThothTarot::Card.all.each do |c|
       puts "#{c.name.upcase}".colorize(:purple)
       puts " Instinct:".colorize(:light_blue) + " #{c.instinct}"
       puts " Purpose:".colorize(:light_blue) + " #{c.purpose}"
